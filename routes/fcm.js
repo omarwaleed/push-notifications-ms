c15567a33207c65f06ca00d1a5b99a83f4d726ef
omarwaleed <omarpersonalprofile@gmail.com>
2018-08-30 00:46:06 +0200

    added firebase fcm messaging routes

--- routes/fcm.js
+++ routes/fcm.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET users listing. */
+const firebase = require('firebase-admin');
+if(process.env.FIREBASE_ENABLED){
+  firebase.initializeApp({
+    credential: firebase.credential.cert('../firebaseCredentials.json')
+  })
+}
+const fcm = firebase.messaging();
+
+/* Specific Devices Message */
 router.get('/', function(req, res, next) {
-  res.sendStatus(502)
+  let { targets, title, body, data } = req.body;
+  fcm.sendToDevice(targets, {
+    notification: {
+      title, body
+    },
+    data
+  })
+  .then((value)=>{
+    console.log(value);
+    res.sendStatus(200);
+  })
+  .catch((err)=>{
+    console.error(err);
+    res.sendStatus(500);
+  })
 });
+
+/* Broadcast Message */
+router.post('/broadcast', function(req, res){
+  let { title, body, data } = req.body;
+  fcm.send({
+    notification: {
+      title, body
+    },
+    data
+  })
+  .then((value)=>{
+    console.log(value);
+    res.sendStatus(200);
+  })
+  .catch((err)=>{
+    console.error(err);
+    res.sendStatus(500);
+  })
+})
+
+router.post('/topic', function(req, res){
+  let { topic, title, body, data } = req.body;
+  fcm.sendToTopic(topic, {
+    notification: {
+      title,
+      body
+    },
+    data
+  })
+  .then((value)=>{
+    console.log(value)
+    res.sendStatus(200);
+  })
+  .catch((err)=>{
+    console.error(err);
+    res.sendStatus(500);
+  })
+})
 
 module.exports = router;
